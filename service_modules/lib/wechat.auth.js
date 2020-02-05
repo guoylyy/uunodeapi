e7bd1383217eb656a3786174b758ad783eead94a
yiliang.guo <guoylyy@gmail.com>
2020-02-06 01:47:02 +0800

    debug

--- service_modules/lib/wechat.auth.js
+++ service_modules/lib/wechat.auth.js
@@ -110,6 +110,50 @@ let getJsApiTicketUrl = (token) => {
 let pub = {};
 
 
+const wechatSignUp  = (userObject) => {
+      if (!_.isPlainObject(userObject)) {
+        return Promise.reject(commonError.PARAMETER_ERROR('注册用户信息不全！'));
+      }
+      winston.error('用户注册信息', userObject);
+
+      //兼容微信最新接口的openId获取任务
+      let openId = userObject.openId;
+      if(_.isNil(openId)){
+        openId = userObject.openid;
+      }
+      let unionId = userObject.unionId;
+      if(_.isNil(unionId)){
+        unionId = userObject.unionid;
+      }
+
+      if (global.IS_DEVLOPMENT_ENVIRONMENT === true) {
+        if (_.isNil(openId)) {
+          return Promise.reject(commonError.PARAMETER_ERROR('注册用户信息有误！'));
+        }
+      } else {
+        if (_.isNil(openId) || _.isNil(unionId)) {
+          return Promise.reject(commonError.PARAMETER_ERROR('注册用户信息有误！'));
+        }
+      }
+
+      // 抽取信息
+      const userItem = {
+        name: userObject.nickname,
+        headImgUrl: userObject.headimgurl,
+        openId: openId,
+        unionid: unionId,
+        sex: userObject.sex,
+        city: userObject.city
+      };
+
+      return userMapper.create(userItem)
+          .then((createdUserItem) => {
+            debug(createdUserItem);
+            return createdUserItem;
+          });
+};
+
+
 /**
  * 获取用户个人信息
  *
@@ -339,7 +383,7 @@ pub.requestUserItemByAuthCode = (appid, appSecret, authCode, userBindType, targe
                 // 如果目标用户为空，则注册
                 if (_.isNil(targetUserItem)) {
                   winston.info("APP WECHAT REGISTER", userObject);
-                  return userService.wechatSignUpPlus(userObject)
+                  return wechatSignUp(userObject)
                       .then((registeredUserItem) => {
                         //  注册第三方用户信息
                         return registerUserBindItem(registeredUserItem)
