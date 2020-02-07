40bcafddadcf19a7dafdc06371e162a53b29dc64
yiliang.guo <guoylyy@gmail.com>
2020-02-07 15:56:25 +0800

    update

--- service_modules/lib/alipay/alipay.payment.js
+++ service_modules/lib/alipay/alipay.payment.js
@@ -49,7 +49,6 @@ const signPayment = (subject, tradeNo, totalAmount) => {
     method: alipayBase.ALIPAY_METHOD_TYPES.CREATE_APP_ORDER,
     format: "JSON",
     charset: "utf-8",
-    _input_charset:"utf-8",
     sign_type: "RSA2",
     timestamp: moment().format("YYYY-MM-DD HH:mm:ss"),
     version: "1.0",
